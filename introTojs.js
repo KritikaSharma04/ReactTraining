2715bb7f4e6ba814794bede8c3399ff0a620ed88
Kritika Sharma <kritikashar00@gmail.com>
2022-11-13 10:19:29 +0545

    Weather API Name added

---
+++ introTojs.js
@@ -0,0 +1,59 @@
+// let name="CSIT"
+// let number=1;
+// let isNice=true;
+// console.log(name);
+
+// for(let name of names)
+// function power(28)
+// {
+//     return 2*28
+// }
+// power(2)
+// console.log(power(2));
+// let result=power();
+
+// const listOfPeople=["name",1,]
+
+// const students={
+//     name:"Kiri",
+//     age:20,
+//     address:"Basundhara"
+// }
+// // object.assign()
+// console.log(students.age);
+
+// // !Jason file
+// const peoples= 
+// [
+// { 
+// name:"Kiri",
+// age:1,
+// },
+// {
+//  ...
+// }
+// ]
+
+// document.rootElement
+
+// const body=document.body
+// const headNode=document.createElement("h1");
+// const textNode=document.createTextNode("tiltle");
+// document.insertBefore()
+
+// document.getElementsById
+
+// document.addEventListener("click",function(event)
+// {
+//     if(event.keycode="S")
+//     {
+//         player.move(left)
+//     }
+// }
+
+// import Index from './Index.html';
+// import React from 'react-js';
+
+
+
+
